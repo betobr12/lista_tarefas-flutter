1dcffbefee0ea853b9cce897fc5278c2d150a172
dinari-roberto-carlos <roberto.carlos@dinaribank.com.br>
2020-10-06 11:22:31 -0300

    completed project

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,55 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = ["Daniel","Marcos"];
+  final _toDoController =
+      TextEditingController(); //Pegar a informação do texto, manipular texto TextField
+
+  List _toDoList = [];
+
+  Map<String, dynamic> _lastRemoved; //mapa do item que acabou de remover para recupera -lo
+  int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    }); //faz a leitura dos dados, função para retornar a leitura dos dados
+  }
+
+  void _addToDo() {
+    setState(() {
+      //setState Atualiza a tela após um evento, nesse caso inserir
+      Map<String, dynamic> newToDo = Map(); //Para manipular o mapeamento JSON
+      newToDo["title"] =
+          _toDoController.text; //Pata pegar o texto do Text field
+      _toDoController.text =
+          ""; // Após adicionar o texto o campo ficará vazio novamente
+      newToDo["ok"] =
+          false; // Inicia como falso, só será verdadeiro quando checarmos a tarefa
+      _toDoList.add(newToDo); //TErminando de adicionar o elemento map
+
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+
+    return null;
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +82,8 @@ class _HomeState extends State<Home> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller:
+                          _toDoController, //controller adicionado para manipulação de texto conforme declarado acima
                       decoration: InputDecoration(
                           labelText: "Nova tarefa",
                           labelStyle: TextStyle(color: Colors.blueAccent)),
@@ -43,25 +93,79 @@ class _HomeState extends State<Home> {
                     color: Colors.blueAccent,
                     child: Text("ADD"),
                     textColor: Colors.white,
-                    onPressed: () {},
+                    onPressed:
+                        _addToDo, //Chamando função para adicionar os itens em JSON
                   )
                 ],
               ),
             ),
             Expanded(
-              child: ListView.builder(
+              
+              child: RefreshIndicator(
+                onRefresh: _refresh,
+                child: ListView.builder(
                   padding: EdgeInsets.only(top: 10.0),
                   itemCount: _toDoList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(_toDoList[index]),
-                    );
-                  }),
-            ),
+                  itemBuilder: buildItem, ),
+            ),),
           ],
         ));
   }
+//metodo responsavel por adicionar os itens na lista
+  Widget buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white,),
+        ),
+      ),
+      direction: DismissDirection.startToEnd, //direção para arrastar o item para exclui -lo da esquerda para direita
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+              _toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          //c é a variavel para alterar o estado do item
+          setState(() {
+            _toDoList[index]["ok"] =
+                c; //altera o estado do item do check
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction){
+          setState(() {
+            _lastRemoved = Map.from(_toDoList[index]);//duplicando o item após remove -lo
+            _lastRemovedPos = index;
+            _toDoList.removeAt(index);
 
+            _saveData();
+
+            final snack = SnackBar(
+              content: Text("Tarefa \"${_lastRemoved["title"]}\" removida"),
+              action: SnackBarAction(label:"Desfazer",
+                onPressed: (){ //função anonima para reincluir o item na lista
+          setState(() {
+             _toDoList.insert(_lastRemovedPos, _lastRemoved);
+              _saveData();//salva os dados recuperados
+              });
+              },),
+              duration: Duration(seconds: 2), //duração do snackbar
+            );
+            Scaffold.of(context).removeCurrentSnackBar();
+            Scaffold.of(context).showSnackBar(snack); //para exibir o snackbar
+
+          });
+      },
+    );
+  }
+//-----------------funções para o armazenamento dos dados JSON--------------------
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
